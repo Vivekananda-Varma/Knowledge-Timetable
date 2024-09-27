5e96c7601320ced6b5cd602a31e64e614ac7051a
Vivekananda Varma <vivnandavarma@gmail.com>
2024-09-27 15:53:27 +0530

    Added scr, docs and dist folders

---
+++ src/js/app.js
@@ -0,0 +1,30 @@
+// Common (required)
+import "./modules/bootstrap";
+import "./modules/theme";
+import "./modules/feather";
+import "./modules/sidebar";
+
+// Common (optional)
+import "./modules/moment";
+import "./modules/dragula";
+import "./modules/notyf";
+
+// Charts (optional)
+import "./modules/chartjs";
+import "./modules/apexcharts";
+
+// Forms (optional)
+import "./modules/daterangepicker"; // requires jQuery
+import "./modules/datetimepicker"; // requires jQuery
+import "./modules/fullcalendar";
+import "./modules/mask"; // requires jQuery
+import "./modules/quill";
+import "./modules/select2"; // requires jQuery
+import "./modules/validation"; // requires jQuery
+import "./modules/wizard"; // requires jQuery
+
+// Maps (optional)
+import "./modules/vector-maps";
+
+// Tables (optional)
+import "./modules/datatables"; // requires jQuery
