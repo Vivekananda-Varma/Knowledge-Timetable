fc1fe6e77cd55cc89f9f510452c9fc325f634267
Vivekananda Varma <vivnandavarma@gmail.com>
2024-10-02 19:45:50 +0530

    Changed the font of the timetable

--- admin/timetable.php
+++ admin/timetable.php
@@ -333,45 +333,49 @@
             </nav>
 
 			<main class="content">
-                <div class="container text-left text-lg-center py-2">
-                    <div class="row row-cols-lg-8 row-cols-1">
-                        <div class="col d-none d-lg-block"></div>
-                        <div class="col period-header d-none d-lg-block">1</div>
-                        <div class="col period-header d-none d-lg-block">2</div>
-                        <div class="col period-header d-none d-lg-block">3</div>
-                        <div class="col period-header d-none d-lg-block">4</div>
-                        <div class="col period-header d-none d-lg-block">5</div>
-                        <div class="col period-header d-none d-lg-block">6</div>
-                        <div class="col period-header d-none d-lg-block">7</div>
-<?php    
-                    for($i = 0; $i < count($timetable); $i++) {
-                        $row = $timetable[$i];
-                        $day = $row['day'];
-                        $periods = $row['periods'];
-?>
-                        <div class="col day"><?= $day ?></div>
-<?php    
-                        for ($j = 0; $j < count($periods); $j++) {
-                            $period = $periods[$j];
-                            $category = $period['category'];
-                            $subject = $period['subject'] != '' ? $period['subject'] : '&nbsp;';
-                            $teacher = $period['teacher'] != '' ? $period['teacher'] : '&nbsp;';
-                            $place = $period['place'];
-?>
-                        <div class="col">
-                            <span class="period">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tools" viewBox="0 0 16 16"><path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3c0-.269-.035-.53-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814L1 0Zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708ZM3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026L3 11Z"/></svg>
-                            </span>
-                            <div class="subject"><?= $subject ?></div><div class="teacher"><?= $teacher ?></div>
-                        </div>
-<?php
-                        }
-                    }
-?>
-
-
+                <div class="container-fluid p-8">
+                    <h1 class="h3 mb-3">Timetable</h1>
+                    <div class="card">
+                        <div class="container text-left text-lg-center py-2">
+                            <div class="row row-cols-lg-8 row-cols-1">
+                                <div class="col d-none d-lg-block"></div>
+                                <div class="col period-header d-none d-lg-block">1</div>
+                                <div class="col period-header d-none d-lg-block">2</div>
+                                <div class="col period-header d-none d-lg-block">3</div>
+                                <div class="col period-header d-none d-lg-block">4</div>
+                                <div class="col period-header d-none d-lg-block">5</div>
+                                <div class="col period-header d-none d-lg-block">6</div>
+                                <div class="col period-header d-none d-lg-block">7</div>
+    <?php    
+                            for($i = 0; $i < count($timetable); $i++) {
+                                $row = $timetable[$i];
+                                $day = $row['day'];
+                                $periods = $row['periods'];
+    ?>
+                                <div class="col day"><?= $day ?></div>
+    <?php    
+                                for ($j = 0; $j < count($periods); $j++) {
+                                    $period = $periods[$j];
+                                    $category = $period['category'];
+                                    $subject = $period['subject'] != '' ? $period['subject'] : '&nbsp;';
+                                    $teacher = $period['teacher'] != '' ? $period['teacher'] : '&nbsp;';
+                                    $place = $period['place'];
+    ?>
+                                <div class="col">
+                                    <span class="period">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-tools" viewBox="0 0 16 16"><path d="M1 0 0 1l2.2 3.081a1 1 0 0 0 .815.419h.07a1 1 0 0 1 .708.293l2.675 2.675-2.617 2.654A3.003 3.003 0 0 0 0 13a3 3 0 1 0 5.878-.851l2.654-2.617.968.968-.305.914a1 1 0 0 0 .242 1.023l3.27 3.27a.997.997 0 0 0 1.414 0l1.586-1.586a.997.997 0 0 0 0-1.414l-3.27-3.27a1 1 0 0 0-1.023-.242L10.5 9.5l-.96-.96 2.68-2.643A3.005 3.005 0 0 0 16 3c0-.269-.035-.53-.102-.777l-2.14 2.141L12 4l-.364-1.757L13.777.102a3 3 0 0 0-3.675 3.68L7.462 6.46 4.793 3.793a1 1 0 0 1-.293-.707v-.071a1 1 0 0 0-.419-.814L1 0Zm9.646 10.646a.5.5 0 0 1 .708 0l2.914 2.915a.5.5 0 0 1-.707.707l-2.915-2.914a.5.5 0 0 1 0-.708ZM3 11l.471.242.529.026.287.445.445.287.026.529L5 13l-.242.471-.026.529-.445.287-.287.445-.529.026L3 15l-.471-.242L2 14.732l-.287-.445L1.268 14l-.026-.529L1 13l.242-.471.026-.529.445-.287.287-.445.529-.026L3 11Z"/></svg>
+                                    </span>
+                                    <div class="subject"><?= $subject ?></div><div class="teacher"><?= $teacher ?></div>
+                                </div>
+    <?php
+                                }
+                            }
+    ?>
+                            </div>
+                        </div>    
                     </div>
                 </div>
+
             </main>
             <footer class="footer">
                 <div class="container-fluid">
