7752ca679c3c7e47eaf0f3f774a0fb492c7d306d
Vivekananda Varma <vivnandavarma@gmail.com>
2024-10-18 13:02:00 +0530

    Added forms to the user detail page

--- admin/modules/users/user_detail.php
+++ admin/modules/users/user_detail.php
@@ -2,14 +2,14 @@
 <html lang="en">
 
 <head>
-	<?php include('templates/head.html'); ?>
+	<?php include 'templates/head.html'; ?>
 </head>
 
 <body data-theme="default" data-layout="fluid" data-sidebar-position="left" data-sidebar-behavior="sticky">
 	<div class="wrapper">
-		<?php include('templates/sidebar.html'); ?>
+		<?php include 'templates/sidebar.html'; ?>
 		<div class="main">
-			<?php include('templates/navbar.html'); ?>
+			<?php include 'templates/navbar.html'; ?>
 			
 			<main class="content">
 				<div class="container-fluid p-0">
@@ -24,44 +24,60 @@
 								  <h5 class="card-title mb-0">Profile Details</h5>
 							   </div>
 							   <div class="card-body text-center">
-								  <img src="../../dist/img/avatars/avatar-4.jpg" alt="Stacie Hall" class="img-fluid rounded-circle mb-2" width="128" height="128" />
+								  <img src="../../images/user-default-profile-pic.jpg" alt="User Default Profile Pic" class="img-fluid rounded-circle mb-2" width="128" height="128">
 								  <h5 class="card-title mb-0">User Name</h5>
 							   </div>
 							   <table class="table table-sm my-2">
 									<tbody>
 										<tr>
 											<th>DOB</th>
-											<td>2005</td>
-											</tr>
-									<tr>
-										<th>Section</th>
-										<td>K1</td>
-									</tr>
-									<tr>
-										<th>Email</th>
-										<td>user@gmail.com</td>
-									</tr>
-									<tr>
-									<th>Phone</th>
-									<td>+1234123123123</td>
-									</tr>
-									<tr>
-									<th>Status</th>
-									<td><span class="badge bg-success">Active</span></td>
-									</tr>
-</tbody>
-							    </table>
+											<td>
+												<div class="col-sm-12">
+													<input type="text" class="form-control" name="dob" value="" placeholder="DOB" />
+												</div>
+											</td>
+										</tr>
+										<tr>
+											<th>Section</th>
+											<td>
+												<div class="col-sm-12">
+													<input type="text" class="form-control" name="section" value="" placeholder="Section" />
+												</div>
+											</td>
+										</tr>
+										<tr>
+											<th>Email</th>
+											<td>
+												<div class="col-sm-12">
+													<input type="email" class="form-control" name="email" value="" placeholder="Email" />
+												</div>
+											</td>
+										</tr>
+										<tr>
+											<th>Phone</th>
+											<td>
+												<div class="col-sm-12">
+													<input type="tel" class="form-control" name="phone" value="" placeholder="Phone" />
+												</div>
+											</td>
+										</tr>
+										<tr>
+											<th>Status</th>
+											<td><span class="badge bg-success m-1">Active</span></td>
+										</tr>
+									</tbody>
+								</table>
 							</div>
 						</div>
 					</div>
 				</div>
 			</main>
 
-			<?php include('templates/footer.html'); ?>
+			<?php include 'templates/footer.html'; ?>
 		</div>
 	</div>
 
-	<?php include('templates/foot.html'); ?>
+	<?php include 'templates/foot.html'; ?>
 
 </body>
 
