7179629ebbb9d98092ca139c73b59278bcca50ac
Shashwat Parhi <shashwat.parhi@gmail.com>
2024-10-21 23:30:27 +0530

    fixed student form layout issue

--- admin/modules/students/detail.php
+++ admin/modules/students/detail.php
@@ -134,12 +134,7 @@
 													<div class="mb-3 col-md-8">
 														<label class="form-label" for="inputAddressLine1">Address Line 1</label>
 														<input type="text" class="form-control" id="inputAddressLine1" placeholder="Apartment, studio, or floor" />
-														<div class="mb-3 col-md-4">
-															&nbsp;
-														</div>
 													</div>
-												</div>
-												<div class="row">
 													<div class="mb-3 col-md-8">
 														<label class="form-label" for="inputAddressLine2">Address Line 2</label>
 														<input type="text" class="form-control" id="inputAddressLine2" placeholder="Street Name" />
